f1b725f03f0e09e3f5ef26d9e5b9bee019dc86d3
affect <afffect@gmail.com>
2015-02-03 16:22:50 +0200

    Initial English import for MM Core + Addons

---
+++ english/upload/admin/language/english/module/multimerch_sellercarousel.php
@@ -0,0 +1,8 @@
+<?php
+
+$_['heading_title']    = '<b>[MultiMerch]</b> Seller Carousel Addon';
+$_['text_module']         = 'Modules';
+
+$_['ms_config_carousel'] = 'Seller carousel';
+
+?>
---
+++ english/upload/catalog/language/english/module/multimerch_sellercarousel.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Language file for MultiMerch Seller Carousel Addon */
+
+$_['ms_carousel_sellers'] = 'Our sellers';
+$_['ms_carousel_view'] = 'View all sellers';
+
+?>
